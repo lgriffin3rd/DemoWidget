12f20a0a9a2edc40f3831105d0568a4046422f8c
Lewis Griffin <lewis.griffin@qnexis.com>
2020-08-03 09:19:53 -0400

    Initial Commit

---
+++ DemoWidget/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DemoWidget
+//
+//  Created by Lewis Griffin on 8/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoWidget/DemoWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoWidgetApp.swift
+//  DemoWidget
+//
+//  Created by Lewis Griffin on 8/3/20.
+//
+
+import SwiftUI
+
+@main
+struct DemoWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
